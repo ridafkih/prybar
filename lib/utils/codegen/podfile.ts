7ccee4def8a43c747b47b4887e629c10c7e65be3
Rida F'kih <hello@rida.dev>
2022-06-15 00:13:41 -0600

    add comment to podfile

--- lib/utils/codegen/podfile.ts
+++ lib/utils/codegen/podfile.ts
@@ -6,6 +6,12 @@ interface ShimPodfileOptions {
   anchor: string;
 }
 
+/**
+ * Shims the Podfile with the provided lines before the provided anchor text.
+ * @param props.content The content of the Podfile.
+ * @param props.pods An array of pod string to add.
+ * @param props.anchor Content of a line to add the pod references before.
+ */
 export const shimPodfile = ({ content, pods, anchor }: ShimPodfileOptions) => {
   const findAnchor = (lineContent: string) => lineContent.includes(anchor);
 
